491a3946fb5cf0392f79b1a9699459376016d0d5
marcondesjr96 <jrmoriaproducoes1996@gmail.com>
2023-07-14 12:35:32 -0300

    adicionando metodo findAll

--- src/main/java/br/com/banco/service/TransferenciaService.java
+++ src/main/java/br/com/banco/service/TransferenciaService.java
@@ -1,4 +1,11 @@
 package br.com.banco.service;
 
+import br.com.banco.dto.TransferenciaResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface TransferenciaService {
+
+    Page<TransferenciaResponse> findAll(Pageable pageable);
+
 }
--- src/main/java/br/com/banco/service/impl/TransferenciaServiceImpl.java
+++ src/main/java/br/com/banco/service/impl/TransferenciaServiceImpl.java
@@ -1,13 +1,32 @@
 package br.com.banco.service.impl;
 
+import br.com.banco.convert.TransferenciaConvert;
+import br.com.banco.dto.TransferenciaResponse;
+import br.com.banco.model.Transferencia;
 import br.com.banco.repository.TransferenciaRepository;
 import br.com.banco.service.TransferenciaService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class TransferenciaServiceImpl implements TransferenciaService {
 
     private final TransferenciaRepository transferenciaRepository;
+
+    @Override
+    public Page<TransferenciaResponse> findAll(Pageable pageable) {
+        Page<Transferencia> transferenciaPage = transferenciaRepository.findAll(pageable);
+        List<TransferenciaResponse> transferenciaResponses = transferenciaPage.getContent().stream()
+                .map(transferencia -> TransferenciaConvert.modelToDto(transferencia))
+                .collect(Collectors.toList());
+
+        return new PageImpl<>(transferenciaResponses, pageable, transferenciaPage.getTotalElements());
+    }
 }
